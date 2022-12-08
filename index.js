5b6ededf4769d169f1e632ff447795eb00cbbdb3
DeclanLacey <declan.lacey@hotmail.com>
2022-12-07 20:33:26 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const burgerMenu = document.getElementById("burger-menu")
+const burgerMenuOpen = document.getElementById("burger-menu-open")
+const burgerNav = document.getElementById("burger-nav")
+const body = document.getElementById("body")
+
+
+
+burgerMenu.addEventListener("click", openBurgerMenu)
+burgerMenuOpen.addEventListener("click", closeBurgerMenu )
+
+
+function openBurgerMenu() {
+    burgerMenu.style.display = "none"
+    burgerMenuOpen.style.display = "block"
+    burgerNav.style.display = "block"
+
+}
+
+function closeBurgerMenu() {
+    burgerMenu.style.display = "block"
+    burgerMenuOpen.style.display = "none"
+    burgerNav.style.display = "none"
+}
